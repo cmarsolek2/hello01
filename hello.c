5b9cdd00df88d0f62cc01dd8f8d1c30978982681
Clare Marsolek <clare.marsolek@huskers.unl.edu>
2019-09-02 13:19:37 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Clare Marsolek
+ * Date: 2019/09/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Clare Marsolek\n");
+
+  return 0;
+}
